c7d9b03c1ef7c7ad49308d359375c4c14a5e3e32
Shantanu Kaushik <54030684+Aloneduckling@users.noreply.github.com>
2024-07-07 13:20:24 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -279,7 +279,7 @@ const IndexPage = ({ data }) => {
 
                 <div className='footer-right'>
                     <a
-                        href='https://drive.google.com/file/d/1oS2E_vmlMPNNcJUp9fDPpG306dhGgmu8/view?usp=sharing'
+                        href='https://drive.google.com/file/d/1L09DSidXgztOxBvfafelq3vh--4LR5ZO/view?usp=sharing'
                         className='footer-right-btn'
                         target='_blank'
                         rel='noreferrer'
